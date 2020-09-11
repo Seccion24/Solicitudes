c67cc30462812a8db02db114885971642ad48218
Seccion24 <70867981+Seccion24@users.noreply.github.com>
2020-09-10 19:46:10 -0500

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -2,18 +2,21 @@ document.querySelector("#submit").addEventListener("click", e => {
   e.preventDefault();
 
   //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+  
   let telefono = "573105010573";
 
   let nombre = document.querySelector("#nombre").value;
   let ficha = document.querySelector("#ficha").value;
-  let Depto_Base = document.querySelector("#C_Base").value;
-  let Desc_Base = document.querySelector("#Desc_Base").value;
+  let depto_base = document.querySelector("#c_base").value;
+  let desc_base = document.querySelector("#desc_base").value;
   let categoria = document.querySelector("#categoria").value;
-  let Depto_Asc = document.querySelector("#C_Asc").value;
-  let Desc_Asc = document.querySelector("#Desc_Asc");
-  let Fecha1 = document.querySelector("#Fecha1");
-  let Fecha2 = document.querySelector("#Fecha2");
-  let Clausula = document.querySelector("#Clausula");
+  let depto_asc = document.querySelector("#c_asc").value;
+  let desc_asc = document.querySelector("#desc_asc").value;
+  let fecha1 = document.querySelector("#fecha1").value;
+  let fecha2 = document.querySelector("#fecha2").value;
+  let clausula = document.querySelector("#clausula").value;
+
+  let resp = document.querySelector("#respuesta");
 
   resp.classList.remove("fail");
   resp.classList.remove("send");
@@ -27,20 +30,20 @@ document.querySelector("#submit").addEventListener("click", e => {
 		*Categoria Base*%0A
 		${categoria}%0A
 		*Clausula*%0A
-		${Clausula}%0A
+		${clausula}%0A
     *Depto Base*%0A
-    ${Depto_Base}%0A
+    ${depto_base}%0A
 		*Desc. Base*%0A
-    ${Desc_Base}%0A
+    ${desc_base}%0A
     *Depto Asc.*%0A
-    ${Depto_Asc}%0A
+    ${depto_asc}%0A
 		*Desc. Asc*%0A
-    ${Desc_Asc}%0A
+    ${desc_asc}%0A
     *Lapso*%0A
-    ${Fecha1} *a* ${Fecha2}%0A`;
+    ${fecha1} *a* ${fecha2}%0A`;
 
 
-  if (nombre === "" || ficha === "" || Clausula === "Selecciona Tipo de Permiso") {
+  if (nombre === "" || clausula === "Selecciona Tipo de Permiso") {
     resp.classList.add("fail");
     resp.innerHTML = `Faltan algunos datos, ${nombre}`;
     return false;
